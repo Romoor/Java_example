567066b06ed0700a97820d3ec5e6715b45654831
Romoor <oliviaromo@icloud.com>
2022-01-05 22:42:56 -0800

    Make java + examples

---
+++ helloworld.java
@@ -0,0 +1,53 @@
+public class helloworld {
+    public static void main(String[] args) {
+        /* This is how you make Hello World! */
+        System.out.println("Hello World :)");
+
+        System.out.println();
+        System.out.println();
+
+        int sum = 4 + 3 * 178;
+        System.out.println(sum);
+
+        System.out.println();
+        System.out.println();
+
+        String i = "string";
+
+        if (sum % 2 == 0) {
+            i = "Even";
+            System.out.println(i);
+        } else {
+            i = "Odd";
+            System.out.println(i);
+
+        }
+
+        System.out.println();
+        System.out.println();
+
+        int five = 5; // int
+        float decimal = 5.99f; // float
+        char A = 'A'; // char
+        String alpha = "abcdefghi"; // String
+        boolean truth = true; // boolean
+
+        System.out.println();
+        System.out.println();
+
+        for (int j = 0; j < 10; j++) {
+            System.out.println(j);
+        }
+
+        System.out.println();
+        System.out.println();
+
+        int m = 3;
+        while (m < 15) {
+            System.out.println(m);
+            m++;
+        }
+
+    }
+
+}
